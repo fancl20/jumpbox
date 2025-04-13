c27adb0070a5959058cb46471dd90f32f15eb4a2
System administrator <fancl20@gmail.com>
2025-04-13 10:21:09 +0000

    set git name/email

--- users/nixos.nix
+++ users/nixos.nix
@@ -1,12 +1,21 @@
 { config, pkgs, ... }:
 {
   programs.fish.enable = true;
-  programs.vim.enable = true;
-  programs.vim.defaultEditor = true;
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+  };
+  programs.git = {
+    enable = true;
+    config = {
+      init.defaultBranch = "main";
+      user = { name = "fancl20"; email = "fancl20@gmail.com"; };
+    };
+  };
   users.users.nixos = {
       shell = pkgs.fish;
       packages = with pkgs; [
-        git dnsutils
+        dnsutils
         google-cloud-sdk talosctl fluxctl terraform kubectl
         (pkgs.writeShellScriptBin "gcloud-login" ''
           gcloud --project=home-servers-275405 auth application-default login
