48a512392240d7c49ae066fcc956361f0f3e2e35
fancl20 <fancl20@gmail.com>
2025-04-15 12:11:04 +0000

    add 1password-cli

--- users/nixos.nix
+++ users/nixos.nix
@@ -16,10 +16,7 @@
       shell = pkgs.fish;
       packages = with pkgs; [
         dnsutils
-        google-cloud-sdk talosctl fluxctl terraform kubectl
-        (pkgs.writeShellScriptBin "gcloud-login" ''
-          gcloud --project=home-servers-275405 auth application-default login
-        '')
+        google-cloud-sdk talosctl fluxcd terraform kubectl _1password-cli
       ];
   };
 }
